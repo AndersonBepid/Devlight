bdc4913f5e6607ff8adb664342eacaaf98f64ba0
Anderson Oliveira <andersonoliveira@MacBook-Pro-de-Anderson.local>
2018-11-08 13:07:38 -0200

    UI header view

---
+++ Devlight/Library/Extensions/UIColor+Colorhistory.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+extension UIColor {
+
+	/**
+	 name: Iron
+	 red: 0.8431372046
+	 green: 0.8391795754
+	 blue: 0.8431508541
+	 alpha: 1.0000000000
+	 hex: #D7D6D7
+	 **/
+
+	static var iron: UIColor {
+		return UIColor(red: 0.8431372046, green: 0.8391795754, blue: 0.8431508541, alpha: 1.0000000000);
+	}
+
+	/**
+	 name: Yellow Sea
+	 red: 0.9996086955
+	 green: 0.6542554498
+	 blue: 0.0442220494
+	 alpha: 1.0000000000
+	 hex: #FFA70B
+	 **/
+
+	static var yellowSea: UIColor {
+		return UIColor(red: 0.9996086955, green: 0.6542554498, blue: 0.0442220494, alpha: 1.0000000000);
+	}
+
+	/**
+	 name: Very Light Gray
+	 red: 0.8156889677
+	 green: 0.8117269874
+	 blue: 0.8157029748
+	 alpha: 1.0000000000
+	 hex: #D0CFD0
+	 **/
+
+	static var veryLightGray: UIColor {
+		return UIColor(red: 0.8156889677, green: 0.8117269874, blue: 0.8157029748, alpha: 1.0000000000);
+	}
+
+}
